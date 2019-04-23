b12a5d5939969c3a4478c0defedc125aa9a350f7
Ussama Azam <nuclearoreo@gmail.com>
2019-04-23 16:49:28 -0400

    some stuff

---
+++ app/screens/home.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+import { Text, View} from 'react-native';
+
+class home extends Component {
+    render() {
+      return (
+        <View>
+          <Text>Home</Text>
+          <Text>To get started, edit App.js</Text>
+        </View>
+      );
+    }
+  }
+
+  export default home;
